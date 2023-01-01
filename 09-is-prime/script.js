e75c6bef36cf1d8fbc9e91fa37f04b30cd511cd6
Sergey Kouzma <sergey.kouzma@gmail.com>
2023-01-01 14:46:09 +0300

    fix

--- 09-is-prime/script.js
+++ 09-is-prime/script.js
@@ -10,12 +10,15 @@
 */
 
 function isPrime(n) {
+    if (n > 1) {
     for (let i = 2; i < n; i++) {
-        if (n % i) {
+        if (n % i === 0) {
             return false;
         }
     }
     return true;
+    }
+    return false;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
