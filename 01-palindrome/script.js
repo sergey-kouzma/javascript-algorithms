ac610e2edc23b60e31cacf5624a19f264acfe56d
Sergey Kouzma <sergey.kouzma@gmail.com>
2023-01-01 13:33:20 +0300

    fix

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -14,8 +14,9 @@
 */
 
 function palindrome(str) {
+    str = str.toLowerCase();
     for (let i = 0; i < str.length; i++) {
-        if (str[i] !== str[str.length - i] - 1) {
+        if (str[i] !== str[str.length - i - 1]) {
             return false;
         }    
     }
